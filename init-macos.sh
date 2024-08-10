a154aae8490634987211123aedc864fb726d887d
Trung Tho <trantrungtho296@gmail.com>
2024-08-10 16:16:29 +0700

    chore: add heml installation

--- init-macos.sh
+++ init-macos.sh
@@ -1,3 +1,4 @@
+#!/usr/sh
 # install brew first
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 brew list iterm2 || brew install --cask iterm2;
@@ -6,7 +7,7 @@ brew list iterm2 || brew install --cask iterm2;
 brew list kubectl || brew install kubectl;
 echo "source <(kubectl completion zsh)" >> ~/.zshrc;
 brew list kubectx || brew install kubectx;
-
+brew list helm || brew install helm;
 
 # install useful applications
 ## via homebrew
@@ -44,7 +45,7 @@ mas list | grep 'Tayasui Sketches' || mas install 1178074963;
 
 # install developer tools
 brew list git || brew install git;
-brew install --cask proxyman
+brew install --cask proxyman;
 
 # install language runtimes
 ## NodeJS
